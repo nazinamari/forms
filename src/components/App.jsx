4e19bc2da25039f3f0f65fa5d73a1cac111979df
nazinamari <nzyame@gmail.com>
2023-01-24 15:53:08 +0000

    forms

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,15 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from "react";
+import Form from "./Form/Form";
+
+export class App extends Component {
+
+  formSubmitHandler = data => {
+    console.log(data);
+  };
+
+  render(){
+    return (
+      <Form onSubmit={this.formSubmitHandler} />
+    )
+  }
+}
